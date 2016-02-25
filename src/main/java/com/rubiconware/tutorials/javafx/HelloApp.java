e73761095e78928db8f2bcdcf82a8dd7861017ec
Jack Givens <jack@givensgang.com>
2016-02-25 12:08:13 -0700

    initial commit

---
+++ src/main/java/com/rubiconware/tutorials/javafx/HelloApp.java
@@ -0,0 +1,50 @@
+package com.rubiconware.tutorials.javafx;
+
+import java.net.URL;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.JavaFXBuilderFactory;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * This class DOES_SOMETHING.
+ * 
+ * <pre>
+ * Revision History
+ * PCR      Version   Author          Date        Comment
+ * CZZZZ    000       jack         Feb 25, 2016     Class Created.
+ * </pre>
+ * 
+ * @author jack
+ * @version {@value #VERSION}
+ */
+public class HelloApp extends Application {
+
+	public static void main(String[] args) {
+		Application.launch(HelloApp.class, args);
+	}
+
+
+
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		// Parent root =
+		// FXMLLoader.load(getClass().getResource("FontFileEditorFX.fxml"));
+		URL location = getClass().getResource("FontFileEditorFX.fxml");
+		FXMLLoader fxmlLoader = new FXMLLoader();
+		fxmlLoader.setLocation(location);
+		fxmlLoader.setBuilderFactory(new JavaFXBuilderFactory());
+		Parent root = (Parent) fxmlLoader.load(location.openStream());
+
+		stage.setTitle("Hello JavaFX");
+		stage.setScene(new Scene(root, 1000, 1000));
+		stage.show();
+
+		HelloApp controller = fxmlLoader.getController();
+		// controller.setup();
+	}
+}
---
+++ src/main/java/com/rubiconware/tutorials/javafx/HelloController.java
@@ -0,0 +1,22 @@
+package com.rubiconware.tutorials.javafx;
+
+
+/**
+ * This class DOES_SOMETHING.
+ * 
+ * <pre>
+ * Revision History
+ * PCR      Version   Author          Date        Comment
+ * CZZZZ    000       jack         Feb 25, 2016     Class Created.
+ * </pre>
+ * 
+ * @author jack
+ * @version {@value #VERSION}
+ */
+public class HelloController {
+	/**
+	 * Version string for this class. Must be incremented with each PCR. Must match version in last
+	 * line of Revision history.
+	 */
+	public static final String VERSION = "000";
+}
